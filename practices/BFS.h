8c54a0937e4bd35ef86c0cecfa6e75eca5a09f2a
cortexmancer <git@thecortexmancer.org>
2024-04-25 11:15:38 -0300

    chore (wip): queue prototype

--- practices/BFS.h
+++ practices/BFS.h
@@ -19,7 +19,21 @@ typedef struct Graph
     struct Vertex* vertices;
 } Graph;
 
+typedef struct {
+    struct Vertex* items;        
+    int capacity;     
+    int front;       
+    int rear;          
+    int size;          
+} Queue;
+
 Graph* initGraph(int vertexCount);
 void freeGraph(Graph* graph);
 void addEdge(Graph* graph, int origin, int destiny, int weight);
 void BFS(Graph* graph, int index);
+
+void initQueue(Queue* q, int size);
+void enqueue(Queue* q, Vertex element);
+void dequeue(Queue* q, Vertex* element);
+static inline int isEmpty(const Queue* q) { return q->size == 0; }
+void freeQueue(Queue* q);
