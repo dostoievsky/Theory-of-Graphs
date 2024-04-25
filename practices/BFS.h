f4f5070bef78b7b5f5759e31aeaecaaa30fde5a8
cortexmancer <git@thecortexmancer.org>
2024-04-25 09:31:18 -0300

    chore: add graphs structs

---
+++ practices/BFS.h
@@ -0,0 +1,18 @@
+typedef struct Edge
+{
+    int destiny;
+    int weight;
+    struct Edge* next;
+} Edge;
+
+typedef struct Vertex
+{
+    struct Edge* edges;
+    int visited;
+} Vertex;
+
+typedef struct Graph 
+{
+    int vertexCount;
+    struct Vertex* vertices;
+} Graph;
