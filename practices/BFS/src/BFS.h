c2f9417288eb628386b9c2a1a8c285679e9c74b8
cortexmancer <git@thecortexmancer.org>
2024-05-26 23:08:33 -0300

    chore: remove stack

--- practices/BFS/src/BFS.h
+++ practices/BFS/src/BFS.h
@@ -27,11 +27,6 @@ typedef struct {
     int size;          
 } Queue;
 
-typedef struct {
-    int *items;
-    int top;
-} Stack;
-
 Graph* initGraph(int vertexCount);
 void freeGraph(Graph** graph);
 void addEdge(Graph* graph, int origin, int destiny, int weight);
@@ -43,8 +38,3 @@ void dequeue(Queue* q, Vertex* element);
 static inline int isQueueEmpty(const Queue* q) { return q->size == 0; }
 void freeQueue(Queue* q);
 
-void initstack(Stack *s);
-static inline int isStackEmpty(Stack *s) { return s->top == -1; }
-void push(Stack *s, int item);
-void pop(Stack *s, int *item);
-void freeStack(Stack *s);
